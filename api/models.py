ea40760906882a3bd0f11627e452833230d7d85f
xolanisiqhelo <siqhelo25@gmail.com>
2019-11-14 13:01:24 +0200

    first for this project

---
+++ api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+
+class User(AbstractUser):
+    username = models.CharField(_('username'),unique=True, max_length=25)
+    email = models.EmailField(blank=True, null=True, max_length=50)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    title = models.CharField(max_length=15)
+    dob = models.DateField()
+    address = models.CharField(max_length=255)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip = models.CharField(max_length=5)
+    photo = models.ImageField(upload_to='uploads', blank=True)
